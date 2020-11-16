841cda5757d1df6cf358c94397158470d8cbad06
Adewale Orotayo <didymus7007@gmail.com>
2020-11-16 19:22:58 +0100

    Added a bootstrap card to display the books

--- main.js
+++ main.js
@@ -11,10 +11,20 @@ Book.prototype.readStatus = function () {
   this.read = !this.read
 }
 
+let book = new Book('J.R.R. Tolkien', 'The Hobbit', 295);
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function showBook() {
+  myLibrary.forEach(book => {
+    
+  });
 }
 
 function removeBookFromLibrary(index) {
   myLibrary.splice(index, 1);
 }
+
+addBookToLibrary(book);
