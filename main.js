df3ad4a9d553740b863e24f5f18cae2266a8a54c
Adewale Orotayo <didymus7007@gmail.com>
2020-11-16 17:38:42 +0100

    Created book constructor and add to library function

---
+++ main.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pageNumber) {
+  this.author = author
+  this.title = title
+  this.pageNumber = pageNumber
+  this.read = false
+}
+
+function addBookToLibrary() {
+
+}
