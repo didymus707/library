ab70cdb32024cff088f751ec072305c9caf04b18
Adewale Orotayo <didymus7007@gmail.com>
2020-11-16 21:04:41 +0100

    Added a change event listener to the form

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 let myLibrary = [];
 let newBook = document.querySelector('.new-book');
 let form = document.querySelector('.form-for-book');
-let title = document.querySelector('input[type=text]');
+let title = document.querySelector('.title');
+let author = document.querySelector('.author');
 let pages = document.querySelector('input[type=number]');
 let read = document.querySelector('input[type=checkbox]');
 
-newBook.addEventListener('click', () => {
-  form.classList.toggle('hide');
-})
-
-function Book(author, title, pageNumber) {
-  this.author = author
+function Book(title, author, pageNumber) {
   this.title = title
+  this.author = author
   this.pageNumber = pageNumber
   this.read = false
 }
@@ -20,7 +17,28 @@ Book.prototype.readStatus = function () {
   this.read = !this.read
 }
 
-let book = new Book('J.R.R. Tolkien', 'The Hobbit', 295);
+newBook.addEventListener('click', () => {
+  form.classList.toggle('hide');
+})
+
+form.addEventListener('change', (e) => {
+  let title, author, pages, status;
+  if (e.target.id === 'title') {
+    title = e.target.value;
+    console.log(title);
+  } else if (e.target.id === 'author') {
+    author = e.target.value;
+    console.log(author);
+  } else if (e.target.id === 'number') {
+    pages = e.target.value;
+    console.log(pages);
+  } else {
+    status = e.target.checked;
+    console.log(status);
+  }
+})
+
+let book = new Book(author, title, pages);
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -37,4 +55,4 @@ function removeBookFromLibrary(index) {
   myLibrary.splice(index, 1);
 }
 
-addBookToLibrary(book);
+console.log(addBookToLibrary(book));
